d698d0105cf52b41db0610795fc1b02d5f7264a2
Yair <yair.notkovich@gmail.com>
2022-12-22 18:57:54 +0200

    DB populated

--- backend/esc/management/commands/populate_flightroutes.py
+++ backend/esc/management/commands/populate_flightroutes.py
@@ -1,25 +1,31 @@
 from esc.models import FlightRoute, Airport, Airline_Company
 from django.core.management.base import BaseCommand
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
-
+import os
 import json
 
 # from flight_app.utils.db_utils import dataManageUtils as dm
-from geopy import distance as D
+# from geopy import distance as D
 
 jsonPath = "mockup data/flight_routes.json"
 
 
 class Command(BaseCommand):
     def insert_data(self):
+        list_len = 0
+        pos = 1
+        failed = 0
+        print_interval = 50
 
         try:
             with open(jsonPath, "r", encoding="utf8") as data:
                 raw_list = json.load(data)
+                list_len = len(raw_list)
         except Exception as e:
             print(e)
             return None
-
+        os.system("cls||clear")
+        print(f"inserting {list_len} airports")
         for route in raw_list:
             try:
                 airline = Airline_Company.objects.get(code__iexact=route["airline"])
@@ -29,9 +35,9 @@ class Command(BaseCommand):
                     0
                 ]
 
-                origin_lat_lon = (origin.lat_decimal, origin.lon_decimal)
-                dest_lat_lon = (destination.lat_decimal, destination.lon_decimal)
-                distance = D.geodesic(origin_lat_lon, dest_lat_lon).km
+                # origin_lat_lon = (origin.lat_decimal, origin.lon_decimal)
+                # dest_lat_lon = (destination.lat_decimal, destination.lon_decimal)
+                # distance = D.geodesic(origin_lat_lon, dest_lat_lon).km
 
                 FlightRoute.objects.get_or_create(
                     airline=airline,
@@ -39,11 +45,15 @@ class Command(BaseCommand):
                     destination=destination,
                     # distance=distance,
                 )
-
+                if pos % print_interval == 0:
+                    os.system("cls||clear")
+                    print(f"progress {int(pos / list_len * 100)}% {pos}")
+                pos += 1
             except Exception as e:
                 print(
                     e,
                 )
+                failed += 1
 
     def handle(self, *args, **options):
         self.insert_data()
